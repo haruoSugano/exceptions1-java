2288f229af2188108fd08d61a86de78cd70b58d9
Helio Haruo <haruosugano@icloud.com>
2021-01-19 20:14:14 -0300

    Good Solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exception.DomainException;
+
+
 public class Reservation {
 	
 	private Integer roomNumber;
@@ -38,19 +41,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) {
 		
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now) ) {
-			return "Reservation dates for update must be future dates.";
+			throw new DomainException("Reservation dates for update must be future dates.");
 		}
 		if(!checkOut.after(checkIn)) {
-			return "Check-out cate must be after check-in date. ";
+			throw new DomainException("Check-out cate must be after check-in date. ");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 		}
 	
 
